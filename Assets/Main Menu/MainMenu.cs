a6aad323b910ef97e66c0a689ee9586c8a7b0486
Sean Lange <sean@webthingee.com>
2018-08-11 17:51:53 -0700

    main menu

---
+++ Assets/Main Menu/MainMenu.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour 
+{
+	private void Awake()
+	{
+		if (!PlayerPrefs.HasKey("MusicVolume"))
+		{
+			PlayerPrefs.SetFloat("MusicVolume", 0.75f);
+		}		
+		
+		if (!PlayerPrefs.HasKey("SFXVolume"))
+		{
+			PlayerPrefs.SetFloat("SFXVolume", 0.75f);
+		}
+	}
+
+
+	private void Update()
+	{
+		if (Input.GetButtonDown("Jump"))
+		{
+			LoadGame();
+		}
+	}
+
+	public void LoadGame()
+	{
+		SceneManager.LoadScene("SampleScene", LoadSceneMode.Single);
+	}
+}
