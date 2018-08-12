b2c6ed3dac54c97797cfbae162db42a9cfa18b22
Sean Lange <sean@webthingee.com>
2018-08-11 19:50:19 -0700

    clean up animation file

--- Assets/Character/PlayerAnimation.cs
+++ Assets/Character/PlayerAnimation.cs
@@ -4,26 +4,26 @@ using UnityEngine;
 public class PlayerAnimation : MonoBehaviour 
 {
 	public GameObject character;
-	Animator animator;
-	CharacterMovement characterMovement;
+	//Animator animator;
+	private CharacterMovement characterMovement;
 
 	public AudioEvent footstep;
 	public float stepSpeed;
 	public AudioSource sm;
 	public bool walking;
-	IEnumerator walkingSounds;
+	private IEnumerator walkingSounds;
 
 	public float previousPosition;
 	[SerializeField] private bool hasFootsteps;
 
-	void Awake ()
+	private void Awake()
 	{
 		characterMovement = GetComponent<CharacterMovement>();
-		animator = GetComponentInChildren<Animator>();
+		//animator = GetComponentInChildren<Animator>();
 		walkingSounds = WalkingSounds();      
 	}
 
-	void Update ()
+	private void Update ()
 	{        
 		//float yAxis = Input.GetAxis("Vertical");
 		ChangeDirection(characterMovement.GetMoveDirection.x);
@@ -31,7 +31,6 @@ public class PlayerAnimation : MonoBehaviour
 		// animator.SetFloat("Forward", Mathf.Abs(characterMovement.GetMoveDirection.x));
 		//anim.SetFloat("Looking", yAxis);
 
-		//walking = Mathf.Abs(characterMovement.GetMoveDirection.x) > 0 ? true : false;
 		if (walking && characterMovement.isGrounded)
 		{
 			StartWalking();
@@ -51,30 +50,31 @@ public class PlayerAnimation : MonoBehaviour
 
 	void ChangeDirection (float fdirection)
 	{        
-		/// Rotate Player
 		if (fdirection > 0) character.transform.eulerAngles = new Vector3(0, 0, 0);
 		if (fdirection < 0) character.transform.eulerAngles = new Vector3(0, 180, 0);
 	}
 
-	public void StartWalking ()
+	private void StartWalking ()
 	{
-		if (!hasFootsteps)
+		if (hasFootsteps) return;
+		hasFootsteps = true;
+		StartCoroutine(walkingSounds);
+	}
+
+	private void StopWalking ()
+	{
+		StopCoroutine(walkingSounds);
+		
+		if (sm != null)
 		{
-			hasFootsteps = true;
-			StartCoroutine(walkingSounds);
+			sm.Stop();
+			sm = null;
 		}
 		
-	}
-    
-	public void StopWalking ()
-	{
-		if (sm != null) sm.Stop();
-		StopCoroutine(walkingSounds);
-		if (sm != null) sm = null;
 		hasFootsteps = false;
 	}
 
-	IEnumerator WalkingSounds ()
+	private IEnumerator WalkingSounds ()
 	{		
 		while (true)
 		{
@@ -87,7 +87,6 @@ public class PlayerAnimation : MonoBehaviour
 			
 			footstep.Play(sm);
 			yield return new WaitForSeconds(stepSpeed);
-
 		}
 	}
 }
